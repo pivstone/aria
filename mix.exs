63471638942bd9cdc6ba7268f6735ffbf082d46a
xbeco <pivstone@gmail.com>
2017-11-27 17:40:13 +0800

    Fix ExCoverage Settings BUG

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule Aria.Mixfile do
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
     ]
   end
 
