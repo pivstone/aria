48ce4b39557e0b04de3a7d3763aade3dbb80dad4
xbeco <pivstone@gmail.com>
2017-11-28 18:58:56 +0800

    Fix unit test BUG

--- apps/core/test/file_driver_test.exs
+++ apps/core/test/file_driver_test.exs
@@ -2,11 +2,10 @@ defmodule FileDriverTest do
   use ExUnit.Case, async: true
 
   test "file move " do
-    src = System.tmp_dir <> "_test1"
-    dist = System.tmp_dir <> "_test2/data"
+    src = Storage.PathSpec.data_dir() <> "/_test1"
+    dist = Storage.PathSpec.data_dir() <> "/_test2/data"
 
     File.mkdir(src)
-    File.mkdir(System.tmp_dir <> "_test2")
     src = src <> "/data"
     File.write(src, "test")
     Storage.FileDriver.move(src, dist)
