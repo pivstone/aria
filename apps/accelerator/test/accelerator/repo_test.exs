b68566105fbfe3aadf331529fbdc7daf60d492ba
pivstone <pivstone@gmail.com>
2018-10-26 17:23:48 +0800

    fix: test case issue

--- apps/accelerator/test/accelerator/repo_test.exs
+++ apps/accelerator/test/accelerator/repo_test.exs
@@ -54,17 +54,21 @@ defmodule Accelerator.RepoTest do
   end
 
   test "get blob II", %{pid: pid, name: name} do
-
+    {:ok, %Response{code: 200} = rsp} = GenServer.call(pid, {:manifest, "latest"})
+    manifest = Poison.decode!(rsp.body)
+    layers = Map.get(manifest, "layers") || Map.fetch!(manifest, "fsLayers")
+    layer = Enum.at(layers, 0)
+    digest = Map.get(layer, "digest") || Map.fetch!(layer, "blobSum")
     auth = GenServer.call(pid, :auth)
-    url = DockerUrl.blobs(name, "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4")
+    url = DockerUrl.blobs(name, digest)
     headers = [{'Authorization', 'Bearer #{auth}'}]
     {:ok, file} = Accelerator.Http.get(url, headers)
     %{size: size} = File.stat! file
     hash = file
            |> File.read!
            |> Accelerator.Comparator.hash
-    assert hash == "a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
-    assert size == 32
+    assert "sha256:#{hash}" == digest
+    assert size > 32
 
   end
 
